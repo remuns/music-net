d00eeabc3a9320662c6a4b7e973b515b58d8734e
Richard Munson <rmunson@ovitz.us>
2021-10-10 11:36:14 -0400

    Added basic interval quality records

---
+++ MusicNet/MusicNet/Internal/IntChecks.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace REMuns.Music.Internal
+{
+    internal static class IntChecks
+    {
+        public static int EnsureArgPositive(int value, string argName)
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException(
+                    $"expected positive value for argument {argName}");
+            }
+            return value;
+        }
+
+        public static int EnsurePropPositive(int value, string propName)
+        {
+            if (value <= 0)
+            {
+                throw new InvalidOperationException(
+                    $"expected positive value for property {propName}");
+            }
+            return value;
+        }
+    }
+}
---
+++ MusicNet/MusicNet/Intervals/Quality.cs
@@ -0,0 +1,105 @@
+﻿using REMuns.Music.Internal;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace REMuns.Music.Intervals
+{
+    #region PIntervals
+    /// <summary>
+    /// Represents a diminished quality of a perfectable interval.
+    /// </summary>
+    public sealed record DiminishedPIntervalQuality(int Degree) : PIntervalQuality
+    {
+        /// <summary>
+        /// The degree of this diminished interval quality.
+        /// </summary>
+        public int Degree
+        {
+            get => _degree;
+            init => _degree = IntChecks.EnsurePropPositive(value, nameof(Degree));
+        }
+        private readonly int _degree = IntChecks.EnsureArgPositive(Degree, nameof(Degree));
+    }
+
+    /// <summary>
+    /// Represents a perfect interval quality.
+    /// </summary>
+    public sealed record PerfectIntervalQuality : PIntervalQuality;
+
+    /// <summary>
+    /// Represents an augmented quality of a perfectable interval.
+    /// </summary>
+    public sealed record AugmentedPIntervalQuality(int Degree) : PIntervalQuality
+    {
+        /// <summary>
+        /// The degree of this augmented interval quality.
+        /// </summary>
+        public int Degree
+        {
+            get => _degree;
+            init => _degree = IntChecks.EnsurePropPositive(value, nameof(Degree));
+        }
+        private readonly int _degree = IntChecks.EnsureArgPositive(Degree, nameof(Degree));
+    }
+
+    /// <summary>
+    /// Represents the quality of a perfectable interval.
+    /// </summary>
+    public abstract record PIntervalQuality;
+    #endregion
+
+    #region NPIntervals
+    /// <summary>
+    /// Represents a diminished quality of a non-perfectable interval.
+    /// </summary>
+    public sealed record DiminishedNPIntervalQuality(int Degree) : NPIntervalQuality
+    {
+        /// <summary>
+        /// The degree of this diminished interval quality.
+        /// </summary>
+        public int Degree
+        {
+            get => _degree;
+            init => _degree = IntChecks.EnsurePropPositive(value, nameof(Degree));
+        }
+        private readonly int _degree = IntChecks.EnsureArgPositive(Degree, nameof(Degree));
+    }
+
+    /// <summary>
+    /// Represents a minor interval quality.
+    /// </summary>
+    public sealed record MinorIntervalQuality : NPIntervalQuality;
+
+    /// <summary>
+    /// Represents a major interval quality.
+    /// </summary>
+    public sealed record MajorIntervalQuality : NPIntervalQuality;
+
+    /// <summary>
+    /// Represents an augmented quality of a non-perfectable interval.
+    /// </summary>
+    public sealed record AugmentedNPIntervalQuality(int Degree) : NPIntervalQuality
+    {
+        /// <summary>
+        /// The degree of this augmented interval quality.
+        /// </summary>
+        public int Degree
+        {
+            get => _degree;
+            init => _degree = IntChecks.EnsurePropPositive(value, nameof(Degree));
+        }
+        private readonly int _degree = IntChecks.EnsureArgPositive(Degree, nameof(Degree));
+    }
+
+    /// <summary>
+    /// Represents the quality of a non-perfectable interval.
+    /// </summary>
+    public abstract record NPIntervalQuality;
+    #endregion
+
+    /// <summary>
+    /// Represents the quality of an interval.
+    /// </summary>
+    public abstract record IntervalQuality;
+}

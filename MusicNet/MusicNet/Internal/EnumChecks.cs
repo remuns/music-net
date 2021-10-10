16b906ea18be6b7606205cd35db0a20a1e747fdb
Richard Munson <rmunson@ovitz.us>
2021-10-10 13:51:04 -0400

    Added a series of records representing simple intervals

---
+++ MusicNet/MusicNet/Internal/EnumChecks.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace REMuns.Music.Internal
+{
+    internal static class EnumChecks
+    {
+        public static TEnum EnsureArgNamed<TEnum>(TEnum value, string argName)
+        where TEnum : struct, Enum
+        {
+            if (!Enum.IsDefined(typeof(TEnum), value))
+            {
+                throw new InvalidEnumArgumentException(
+                    $"expected named value of type {nameof(TEnum)} for argument \"{argName}\"");
+            }
+            return value;
+        }
+
+        public static TEnum EnsurePropNamed<TEnum>(TEnum value, string propName)
+        where TEnum : struct, Enum
+        {
+            if (!Enum.IsDefined(typeof(TEnum), value))
+            {
+                throw new InvalidOperationException(
+                    $"expected named value of type {nameof(TEnum)} for property \"{propName}\"");
+            }
+            return value;
+        }
+
+    }
+}
---
+++ MusicNet/MusicNet/Intervals/Simple.cs
@@ -0,0 +1,50 @@
+﻿using REMuns.Music.Internal;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace REMuns.Music.Intervals
+{
+    /// <summary>
+    /// Represents a perfectable simple interval.
+    /// </summary>
+    public sealed record PSimpleInterval(
+        PIntervalQuality Quality, SimplePIntervalNumberName NumberName)
+    : SimpleInterval
+    {
+        /// <summary>
+        /// The name of the number of this perfectable simple interval.
+        /// </summary>
+        public SimplePIntervalNumberName NumberName
+        {
+            get => _numberName;
+            init => _numberName = EnumChecks.EnsurePropNamed(value, nameof(NumberName));
+        }
+        private readonly SimplePIntervalNumberName _numberName
+            = EnumChecks.EnsureArgNamed(NumberName, nameof(NumberName));
+    }
+
+    /// <summary>
+    /// Represents a non-perfectable simple interval.
+    /// </summary>
+    public sealed record NPSimpleInterval(
+        PIntervalQuality Quality, SimpleNPIntervalNumberName NumberName)
+    : SimpleInterval
+    {
+        /// <summary>
+        /// The name of the number of this non-perfectable simple interval.
+        /// </summary>
+        public SimpleNPIntervalNumberName NumberName
+        {
+            get => _numberName;
+            init => _numberName = EnumChecks.EnsurePropNamed(value, nameof(NumberName));
+        }
+        private readonly SimpleNPIntervalNumberName _numberName
+            = EnumChecks.EnsureArgNamed(NumberName, nameof(NumberName));
+    }
+
+    /// <summary>
+    /// Represents a simple interval (less than an octave in number).
+    /// </summary>
+    public abstract record SimpleInterval;
+}

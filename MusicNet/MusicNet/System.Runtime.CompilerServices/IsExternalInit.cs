48a964d4d57173f6405f17c26d2ff6f30a8a54f4
Richard Munson <rmunson@ovitz.us>
2021-10-10 11:21:49 -0400

    Added `IsExternalInit` to allow init-only setters in .NET standard 2.0

---
+++ MusicNet/MusicNet/System.Runtime.CompilerServices/IsExternalInit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System.Runtime.CompilerServices
+{
+    /// <summary>
+    /// Added class to allow init-only setters in .NET standard 2.0.
+    /// </summary>
+    internal class IsExternalInit
+    {
+        //
+    }
+}

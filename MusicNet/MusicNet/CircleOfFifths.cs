f8b95ea4c4db30853d16ed2ca19b2540ba7c5dfe
Richard Munson <rmunson@ovitz.us>
2021-10-10 18:37:00 -0400

    Added `CircleOfFifths` class
    This contains static methods pertaining to the circle of fifths.  This commit adds a circle of fifths comparison for intervals.

---
+++ MusicNet/MusicNet/CircleOfFifths.cs
@@ -0,0 +1,74 @@
+﻿using REMuns.Music.Intervals;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace REMuns.Music
+{
+    /// <summary>
+    /// Contains functionality relating to the circle of fifths.
+    /// </summary>
+    public static class CircleOfFifths
+    {
+        /// <summary>
+        /// Compares the two <see cref="SimpleInterval"/> values supplied in relation to their
+        /// position on the circle of fifths.
+        /// </summary>
+        /// <param name="lhs"></param>
+        /// <param name="rhs"></param>
+        /// <returns></returns>
+        public static int CompareSimpleIntervals(SimpleInterval lhs, SimpleInterval rhs)
+            => IntValue(lhs).CompareTo(IntValue(rhs));
+
+        private static int IntValue(SimpleInterval interval)
+        {
+            // Get the distance of the interval from perfect or major along the circle of
+            // fifths to get the value
+            int qualityIntValue, numberIntValue;
+            switch (interval)
+            {
+                case PSimpleInterval(var quality, var number):
+                    qualityIntValue = quality switch
+                    {
+                        DiminishedPIntervalQuality(var degree) => -degree,
+                        PerfectIntervalQuality => 0,
+                        AugmentedPIntervalQuality(var degree) => degree,
+                    };
+
+                    numberIntValue = number switch
+                    {
+                        PSimpleIntervalNumberName.Unison => 0,
+                        PSimpleIntervalNumberName.Fourth => -1,
+                        PSimpleIntervalNumberName.Fifth => 1,
+                    };
+
+                    break;
+
+                case NPSimpleInterval(var quality, var number):
+                    qualityIntValue = quality switch
+                    {
+                        DiminishedNPIntervalQuality(var degree) => -degree - 1,
+                        MinorIntervalQuality => -1,
+                        MajorIntervalQuality => 0,
+                        AugmentedNPIntervalQuality(var degree) => degree,
+                    };
+
+                    numberIntValue = number switch
+                    {
+                        NPSimpleIntervalNumberName.Second => 2,
+                        NPSimpleIntervalNumberName.Third => 4,
+                        NPSimpleIntervalNumberName.Sixth => 3,
+                        NPSimpleIntervalNumberName.Seventh => 5,
+                    };
+
+                    break;
+
+                default:
+                    // Should never happen
+                    throw new NotImplementedException();
+            }
+            
+            return numberIntValue + qualityIntValue * 7;
+        }
+    }
+}
---
+++ MusicNet/MusicNetTest/CircleOfFifthsTest.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using REMuns.Music.Intervals;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace REMuns.Music.Test
+{
+    [TestClass]
+    public class CircleOfFifthsTest
+    {
+        /// <summary>
+        /// Tests that intervals compare as equal to themselves.
+        /// </summary>
+        [TestMethod]
+        public void TestSimpleIntervalComparison_Trivial()
+        {
+            Assert.AreEqual(0, CircleOfFifths.CompareSimpleIntervals(
+                SimpleInterval.Major().Second(),
+                SimpleInterval.Major().Second()));
+
+            Assert.AreEqual(0, CircleOfFifths.CompareSimpleIntervals(
+                SimpleInterval.Minor().Sixth(),
+                SimpleInterval.Minor().Sixth()));
+
+            Assert.AreEqual(0, CircleOfFifths.CompareSimpleIntervals(
+                SimpleInterval.Augmented(4).Third(),
+                SimpleInterval.Augmented(4).Third()));
+
+            Assert.AreEqual(0, CircleOfFifths.CompareSimpleIntervals(
+                SimpleInterval.Diminished(2).Fourth(),
+                SimpleInterval.Diminished(2).Fourth()));
+        }
+
+        /// <summary>
+        /// Tests comparisons between intervals that are not the same.
+        /// </summary>
+        [TestMethod]
+        public void TestSimpleIntervalComparison_NonTrivial()
+        {
+            Assert.AreEqual(-1, CircleOfFifths.CompareSimpleIntervals(
+                SimpleInterval.Minor().Second(),
+                SimpleInterval.Perfect().Unison()));
+
+            Assert.AreEqual(1, CircleOfFifths.CompareSimpleIntervals(
+                SimpleInterval.Augmented().Fourth(),
+                SimpleInterval.Major().Seventh()));
+
+            // Are enharmonically equal
+            Assert.AreEqual(1, CircleOfFifths.CompareSimpleIntervals(
+                SimpleInterval.Augmented().Fourth(),
+                SimpleInterval.Diminished().Fifth()));
+
+            Assert.AreEqual(-1, CircleOfFifths.CompareSimpleIntervals(
+                SimpleInterval.Major().Second(),
+                SimpleInterval.Major().Third()));
+
+            Assert.AreEqual(-1, CircleOfFifths.CompareSimpleIntervals(
+                SimpleInterval.Minor().Seventh(),
+                SimpleInterval.Perfect().Fourth()));
+
+            Assert.AreEqual(1, CircleOfFifths.CompareSimpleIntervals(
+                SimpleInterval.Major().Sixth(),
+                SimpleInterval.Perfect().Fifth()));
+        }
+    }
+}

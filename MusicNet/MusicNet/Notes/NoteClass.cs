4f36231abbece4e5147e7ae42cbe9494a2beff60
Richard Munson <rmunson@ovitz.us>
2021-10-13 09:51:39 -0400

    Added a `NoteClass` record storing a note letter and accidental

---
+++ MusicNet/MusicNet/Notes/NoteClass.cs
@@ -0,0 +1,87 @@
+﻿using REMuns.Music.Internal;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace REMuns.Music.Notes
+{
+    /// <summary>
+    /// Represents a class of notes, ignoring octaves.
+    /// </summary>
+    public sealed record NoteClass(NoteLetter Letter, int Accidental = 0)
+    {
+        /// <summary>
+        /// Gets whether or not this note is sharp.
+        /// </summary>
+        public bool IsSharp => Accidental > 0;
+
+        /// <summary>
+        /// Gets whether or not this note is natural.
+        /// </summary>
+        public bool IsNatural => Accidental == 0;
+
+        /// <summary>
+        /// Gets whether or not this note is flat.
+        /// </summary>
+        public bool IsFlat => Accidental < 0;
+
+        /// <summary>
+        /// The letter of the note class this object represents.
+        /// </summary>
+        public NoteLetter Letter
+        {
+            get => _letter;
+            init => _letter = EnumChecks.EnsureArgNamed(Letter, nameof(Letter));
+        }
+        private readonly NoteLetter _letter = EnumChecks.EnsureArgNamed(Letter, nameof(Letter));
+
+        /// <summary>
+        /// Gets a builder object that can be used to construct an 'A' note with a
+        /// given accidental.
+        /// </summary>
+        /// <returns></returns>
+        public static NoteClassBuilder A() => new() { Letter = NoteLetter.A };
+
+        /// <summary>
+        /// Gets a builder object that can be used to construct a 'B' note with a
+        /// given accidental.
+        /// </summary>
+        /// <returns></returns>
+        public static NoteClassBuilder B() => new() { Letter = NoteLetter.B };
+
+        /// <summary>
+        /// Gets a builder object that can be used to construct a 'C' note with a
+        /// given accidental.
+        /// </summary>
+        /// <returns></returns>
+        public static NoteClassBuilder C() => new() { Letter = NoteLetter.C };
+
+        /// <summary>
+        /// Gets a builder object that can be used to construct a 'D' note with a
+        /// given accidental.
+        /// </summary>
+        /// <returns></returns>
+        public static NoteClassBuilder D() => new() { Letter = NoteLetter.D };
+
+        /// <summary>
+        /// Gets a builder object that can be used to construct an 'E' note with a
+        /// given accidental.
+        /// </summary>
+        /// <returns></returns>
+        public static NoteClassBuilder E() => new() { Letter = NoteLetter.E };
+
+        /// <summary>
+        /// Gets a builder object that can be used to construct an 'F' note with a
+        /// given accidental.
+        /// </summary>
+        /// <returns></returns>
+        public static NoteClassBuilder F() => new() { Letter = NoteLetter.F };
+
+        /// <summary>
+        /// Gets a builder object that can be used to construct a 'G' note with a
+        /// given accidental.
+        /// </summary>
+        /// <returns></returns>
+        public static NoteClassBuilder G() => new() { Letter = NoteLetter.G };
+    }
+}
---
+++ MusicNet/MusicNet/Notes/NoteClassBuilder.cs
@@ -0,0 +1,57 @@
+﻿using REMuns.Music.Internal;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace REMuns.Music.Notes
+{
+    /// <summary>
+    /// An object that can build a <see cref="NoteClass"/> with a
+    /// predefined <see cref="NoteLetter"/>.
+    /// </summary>
+    public readonly struct NoteClassBuilder
+    {
+        /// <summary>
+        /// The note letter to assign to the <see cref="NoteClass"/> being constructed.
+        /// </summary>
+        public NoteLetter Letter { get; init; }
+
+        /// <summary>
+        /// Constructs a new <see cref="NoteClass"/> with the predefined note letter and the
+        /// given sharp degree.
+        /// </summary>
+        /// <param name="Degree"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException">
+        /// <paramref name="Degree"/> was non-positive.
+        /// </exception>
+        public NoteClass Sharp(int Degree = 1)
+            => new(Letter, IntChecks.EnsureArgPositive(Degree, nameof(Degree)));
+
+        /// <summary>
+        /// Constructs a new <see cref="NoteClass"/> with the predefined note letter and a
+        /// natural accidental.
+        /// </summary>
+        /// <returns></returns>
+        public NoteClass Natural() => new(Letter);
+
+        /// <summary>
+        /// Constructs a new <see cref="NoteClass"/> with the predefined note letter and the
+        /// given flat degree.
+        /// </summary>
+        /// <param name="Degree"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException">
+        /// <paramref name="Degree"/> was non-positive.
+        /// </exception>
+        public NoteClass Flat(int Degree = 1)
+            => new(Letter, -IntChecks.EnsureArgPositive(Degree, nameof(Degree)));
+
+        /// <summary>
+        /// Implicitly converts a <see cref="NoteClassBuilder"/> to a natural note with the
+        /// letter it wraps.
+        /// </summary>
+        /// <param name="builder"></param>
+        public static implicit operator NoteClass(NoteClassBuilder builder) => new(builder.Letter);
+    }
+}

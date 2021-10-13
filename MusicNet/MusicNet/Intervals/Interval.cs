eaa661b8ef4f20e73563a670d4c711da7a30b0ed
Richard Munson <rmunson@ovitz.us>
2021-10-13 09:04:29 -0400

    More refactoring of `Interval.Octaves` property
    * Fixes a typo in the constructor argument name
    * Adds a default value of `0` for the constructor argument (indicating a strictly simple interval)

--- MusicNet/MusicNet/Intervals/Interval.cs
+++ MusicNet/MusicNet/Intervals/Interval.cs
@@ -8,7 +8,7 @@ namespace REMuns.Music.Intervals
     /// <summary>
     /// Represents a musical interval.
     /// </summary>
-    public sealed record Interval(SimpleInterval Base, int Octave)
+    public sealed record Interval(SimpleInterval Base, int Octaves = 0)
     {
         /// <summary>
         /// Gets whether or not this interval represents a simple interval (i.e. has an octave
@@ -27,10 +27,10 @@ namespace REMuns.Music.Intervals
         /// </summary>
         public int Octaves
         {
-            get => _octave;
-            init => _octave = IntChecks.EnsurePropNonNegative(value, nameof(Octaves));
+            get => _octaves;
+            init => _octaves = IntChecks.EnsurePropNonNegative(value, nameof(Octaves));
         }
-        private readonly int _octave = IntChecks.EnsureArgNonNegative(Octave, nameof(Octave));
+        private readonly int _octaves = IntChecks.EnsureArgNonNegative(Octaves, nameof(Octaves));
 
         /// <summary>
         /// Gets an <see cref="Interval"/> equivalent to this one with the specified number

8b9d5afc7a4af661a4135fa972ebd0aedbb79e6d
Richard Munson <rmunson@ovitz.us>
2021-10-13 09:33:51 -0400

    Added an enum for note letters

---
+++ MusicNet/MusicNet/Notes/NoteLetter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace REMuns.Music.Notes
+{
+    /// <summary>
+    /// Represents the letter name of a note.
+    /// </summary>
+    public enum NoteLetter
+    {
+        /// <summary>
+        /// Represents an 'A' note.
+        /// </summary>
+        A,
+
+        /// <summary>
+        /// Represents a 'B' note.
+        /// </summary>
+        B,
+
+        /// <summary>
+        /// Represents a 'C' note.
+        /// </summary>
+        C,
+
+        /// <summary>
+        /// Represents a 'D' note.
+        /// </summary>
+        D,
+
+        /// <summary>
+        /// Represents an 'E' note.
+        /// </summary>
+        E,
+
+        /// <summary>
+        /// Represents an 'F' note.
+        /// </summary>
+        F,
+
+        /// <summary>
+        /// Represents a 'G' note.
+        /// </summary>
+        G,
+    }
+}

2d9a95796e471a7d47c9f19f9aadfe866338f23a
Richard Munson <rmunson@ovitz.us>
2021-10-17 11:12:33 -0400

    Added struct for representing an interval equipped with a sign
    This cannot be a record, since allowing up and down directed intervals to be constructed wrapping unisons would introduce ambiguity.  An upwards-facing interval wrapping a unison and a downwards-facing interval wrapping the same unison inverted should be considered equal.
    
    Update MusicNet.csproj

---
+++ MusicNet/MusicNet/Intervals/DirectedInterval.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace REMuns.Music.Intervals
+{
+    /// <summary>
+    /// Represents an interval equipped with a direction.
+    /// </summary>
+    /// <remarks>
+    /// The default value of this struct wraps a perfect unison.
+    /// </remarks>
+    public readonly struct DirectedInterval : IEquatable<DirectedInterval>
+    {
+        /// <summary>
+        /// The value of the interval being represented.
+        /// </summary>
+        public Interval Value => _value ?? Interval.PerfectUnison;
+        private readonly Interval _value;
+
+        /// <summary>
+        /// The sign of the interval being represented.
+        /// </summary>
+        public int Sign { get; }
+
+        /// <summary>
+        /// Constructs a new <see cref="DirectedInterval"/> representing the interval passed in
+        /// with a positive direction.
+        /// </summary>
+        /// <param name="Value"></param>
+        /// <returns></returns>
+        public static DirectedInterval Up(Interval Value) => new(Value, 1);
+
+        /// <summary>
+        /// Constructs a new <see cref="DirectedInterval"/> representing the interval passed in
+        /// with a negative direction.
+        /// </summary>
+        /// <param name="Value"></param>
+        /// <returns></returns>
+        public static DirectedInterval Down(Interval Value) => new(Value, -1);
+
+        private DirectedInterval(Interval Value, int Sign)
+        {
+            if (Value.Base.Number == 1 && Value.Octaves == 0) // Is a unison
+            {
+                if (Sign < 0)
+                {
+                    // Invert the value to remove ambiguity
+                    Value = Value with { Base = Value.Base.Inverted() };
+                }
+                Sign = 0; // Unisons have no sign
+            }
+
+            this._value = Value;
+            this.Sign = Sign;
+        }
+
+        /// <summary>
+        /// Determines if the two <see cref="DirectedInterval"/> instances are equal.
+        /// </summary>
+        /// <param name="lhs"></param>
+        /// <param name="rhs"></param>
+        /// <returns></returns>
+        public static bool operator ==(DirectedInterval lhs, DirectedInterval rhs)
+            => lhs.Equals(rhs);
+
+        /// <summary>
+        /// Determines if the two <see cref="DirectedInterval"/> instances are not equal.
+        /// </summary>
+        /// <param name="lhs"></param>
+        /// <param name="rhs"></param>
+        /// <returns></returns>
+        public static bool operator !=(DirectedInterval lhs, DirectedInterval rhs)
+            => !lhs.Equals(rhs);
+
+        /// <inheritdoc cref="object.Equals(object)"/>
+        public override bool Equals(object obj) => obj switch
+        {
+            DirectedInterval di => this.Equals(di),
+            _ => false,
+        };
+
+        /// <inheritdoc cref="object.GetHashCode"/>
+        public override int GetHashCode() => HashCode.Combine(Value, Sign);
+
+        /// <inheritdoc cref="IEquatable{T}.Equals(T)"/>
+        public bool Equals(DirectedInterval other)
+        {
+            return Value == other.Value && Sign == other.Sign;
+        }
+
+        /// <inheritdoc cref="object.ToString"/>
+        public override string ToString()
+            => $"{nameof(DirectedInterval)}.{(Sign < 0 ? "Down" : "Up")}({Value})";
+    }
+}
---
+++ MusicNet/MusicNetTest/Intervals/DirectedIntervalTest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using REMuns.Music.Intervals;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace REMuns.Music.Test.Intervals
+{
+    /// <summary>
+    /// Tests for the <see cref="DirectedInterval"/> struct.
+    /// </summary>
+    [TestClass]
+    public class DirectedIntervalTest
+    {
+        /// <summary>
+        /// Tests that construction occurs properly when a unison is passed in as the value.
+        /// </summary>
+        [TestMethod]
+        public void TestUnisonConstruction()
+        {
+            var augUnison = SimpleInterval.Augmented().Unison();
+            var upDirectedInterval = DirectedInterval.Up(augUnison);
+            var downDirectedInterval = DirectedInterval.Down(augUnison);
+
+            // The signs of directed intervals wrapping unisons should always be 0
+            Assert.AreEqual(0, upDirectedInterval.Sign);
+            Assert.AreEqual(0, downDirectedInterval.Sign);
+
+            // Test that up unison wraps the same interval
+            Assert.AreEqual(new Interval(augUnison), upDirectedInterval.Value);
+
+            // Test that down unison wraps the inverted interval
+            Assert.AreEqual(
+                new Interval(SimpleInterval.Diminished().Unison()), downDirectedInterval.Value);
+        }
+    }
+}

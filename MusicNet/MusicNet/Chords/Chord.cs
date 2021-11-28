dcbca5f9dc6805f85decedb3546f5f045ff2bc28
Richard Munson <rmunson@ovitz.us>
2021-11-28 15:23:26 -0500

    Added `Chord` record

---
+++ MusicNet/MusicNet/Chords/Chord.cs
@@ -0,0 +1,48 @@
+﻿using REMuns.Music.Notes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace REMuns.Music.Chords
+{
+    /// <summary>
+    /// Represents a chord.
+    /// </summary>
+    /// <param name="Root"></param>
+    /// <param name="Schema"></param>
+    public sealed record Chord(NoteClass Root, IChordSchema Schema)
+    {
+        /// <summary>
+        /// Gets the base note class of the chord.
+        /// </summary>
+        public NoteClass Base => NoteClasses.First();
+
+        /// <summary>
+        /// Gets the note classes of the chord.
+        /// </summary>
+        public IEnumerable<NoteClass> NoteClasses
+        {
+            get
+            {
+                if (Schema is InversionChordSchema ics)
+                {
+                    // Handle the inversion properly by separating the base from the root
+                    var baseNote = Root - ics.Root;
+
+                    foreach (var interval in ics.Intervals)
+                    {
+                        yield return baseNote + interval;
+                    }
+                }
+                else
+                {
+                    foreach (var interval in Schema.Intervals)
+                    {
+                        yield return Root + interval;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ MusicNet/MusicNetTest/Chords/ChordTest.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using REMuns.Music.Chords;
+using REMuns.Music.Notes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace REMuns.Music.Test.Chords
+{
+    /// <summary>
+    /// Tests for the <see cref="Chord"/> record.
+    /// </summary>
+    [TestClass]
+    public class ChordTest
+    {
+        /// <summary>
+        /// Tests that the <see cref="Chord.NoteClasses"/> returns the note classes of the chord
+        /// in the correct order.
+        /// </summary>
+        [TestMethod]
+        public void TestNoteClasses()
+        {
+            var rootPosChord = new Chord(NoteClass.A(), TriadSchema.Major);
+            Assert.IsTrue(
+                new[] { NoteClass.A(), NoteClass.C().Sharp(), NoteClass.E() }
+                    .SequenceEqual(rootPosChord.NoteClasses));
+
+            var invertedChord = new Chord(
+                NoteClass.C(), new InversionChordSchema(TriadSchema.Minor, 1));
+            Assert.IsTrue(
+                new[] { NoteClass.E().Flat(), NoteClass.G(), NoteClass.C() }
+                    .SequenceEqual(invertedChord.NoteClasses));
+        }
+    }
+}
